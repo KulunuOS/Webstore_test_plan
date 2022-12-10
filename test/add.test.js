66bb3b239bc9c1adfb0344ddeb1331da2b673abd
KulunuOS <kulunuds@gmail.com>
2022-12-10 11:17:01 +0200

    Added test script, tests with github actions

--- test/add.test.js
+++ test/add.test.js
@@ -1,13 +1,15 @@
 // node 12 and up
 //import chai from "chai"
-//import sum from "../sum.js"
+//import add from "../src/add.js"
+
 // below for node 10
 var chai = require("chai")
-var sum = require ("../src/add.js")
+var add = require ("../add.js")
+
 const expect = chai.expect
 
 
-describe("Sum", () => {
+describe("add", () => {
  it("adds positive number to positive number", () =>{
      expect(add(3,6)).to.equal(9)
  });
